35ed7f549c609d68b54f7042c39d0212f5ce36ee
Mansi30032024 <165551405+Mansi30032024@users.noreply.github.com>
2025-06-23 12:59:01 +0000

    Day1

--- Day1.cpp
+++ Day1.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
+int binarySearch(vector<int>arr, int n , int k){
+  int low=0, high=n-1;
+  while(low<=high){
+    int mid =(low+high)/2;
+    if(arr[mid]==k){
+      return mid;
+    }
+    else  if(arr[mid]<k){
+      //search in right half
+      low=mid+1;
+    }
+    else{
+      //search in left half
+      high = mid-1;
+    }
+  }
+  return -1;
+}
  int main(){
-  cout<<"HELOO"<<endl;
+     vector<int> arr={2,8,10,15,37,80};
+     
+     int n = arr.size();
+    cout<< binarySearch(arr,n,37)<<endl;
+    cout<< binarySearch(arr,n,8)<<endl;
   return 0;
  }
  
--- testing.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
- int main(){
-  cout<<"HELO, This is for testing!"<<endl;
-  return 0;
-  
- }
- 
