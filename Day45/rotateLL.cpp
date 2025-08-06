f52bea5cce5e43cf699ce50107de7ce1973cfdcd
Mansiarora123 <aroramansi309@gmail.com>
2025-08-06 21:07:07 +0530

    day45

---
+++ Day45/rotateLL.cpp
@@ -0,0 +1,45 @@
+//61.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* findNthNode(ListNode* temp, int k){
+      int cnt=1;
+      while(temp != NULL){
+        if(cnt==k) return  temp;
+        cnt++;
+        temp = temp->next;
+
+      }
+      return temp;
+}
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || k==0) return head;
+       ListNode* tail = head;
+       int len=1;
+       //find len and tail of LL
+       while(tail->next != NULL){
+          tail= tail->next;
+        len++;
+       }
+       if(k % len == 0) return head;
+       k = k%len; //if k's value is multiple of k --> LL-->original configuration
+       tail->next = head;
+
+       ListNode* newLastNode = findNthNode(head, len-k);
+      
+       head = newLastNode->next;
+      
+       newLastNode->next = NULL;
+      
+       return head;
+    }
+};
