7515a1d6b81c964dcfc93b29c1eac21416597f5a
Mansiarora123 <aroramansi309@gmail.com>
2025-08-16 12:55:14 +0530

    day54

--- Day54/validParenthesis.cpp
+++ Day54/validParenthesis.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-
-        stack<char>st; 
-        for(auto it: s) {
-            //opening
-            if(it=='(' || it=='{' || it == '[') st.push(it); 
-            //closing   
-            else {
-                if(st.size() == 0) return false; 
-                char ch = st.top(); 
-                st.pop(); 
-                //matching
-         if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
-                else return false;
+        
+     stack<char> st;
+        for(int i=0; i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
             }
+            else{
+              if( st.empty()) return false;
+              char ch = st.top();
+              st.pop();
+              //s[i]-->closing , st-->opening of same typr
+              if((s[i]==')' && ch =='(' ) || (s[i]=='}' && ch =='{') || (s[i]==']' && ch =='[')){
+                continue;
+              }
+              else{
+                return false;
+              }
+                  
+            } 
+           
         }
         return st.empty();
-        
     }
 };
-//tc:O(N)
-//Sc:O(N)
+//TC: O(n)
+//SC: O(n) in worst case, if all opening brackets are present
