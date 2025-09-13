6f7b71d4a0ac97056a11a14017e607356cc9478f
Mansiarora123 <aroramansi309@gmail.com>
2025-09-13 13:08:25 +0530

    day71

---
+++ Day71/astCollision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
+      vector<int> st;
+      for(int i=0;i<n;i++){
+        if(a[i]>0){ st.push_back(a[i]);}
+        //negative
+        else{
+            while(!st.empty() && st.back()>0 && st.back() < abs(a[i])){
+                st.pop_back();
+            }
+            if(!st.empty() && st.back() == abs(a[i])){
+                st.pop_back();
+            }
+            else if(st.empty() || st.back()<0){
+                st.push_back(a[i]);
+            }
+        }
+      }
+        return st;
+    }
+};
